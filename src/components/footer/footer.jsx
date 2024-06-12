80140fa53995f11613d30572caeb8a71d5f31042
ChristianGalindo10 <cygc1@hotmail.com>
2024-06-12 09:33:56 -0500

    FEAT: Footer y arreglos navbar

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,122 +1,63 @@
 import React, { useState, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import celsia from "../../assets/icons/celsia.png";
+import ecopetrol from "../../assets/icons/ecopetrol.png";
+import enel from "../../assets/icons/enel.png";
+import epm from "../../assets/icons/epm.png";
+import isagen from "../../assets/icons/isagen.png";
 import "./footer.css";
 
 const Footer = () => {
-  const [linkTo, setLinkTo] = useState("/");
-  const [isLeftSide, setIsLeftSide] = useState(true);
-
-  useEffect(() => {
-    // Actualizar el destino del enlace según la posición del puntero
-    setLinkTo(
-      isLeftSide
-        ? "/"
-        : "/construyendo civilidad/ La responsabilidad social de la Sociedad de Mejoras y Ornato de Bogotá"
-    );
-  }, [isLeftSide]);
-  const handleMouseMove = (event) => {
-    const image = event.target;
-    const clickX = event.clientX - image.getBoundingClientRect().left;
-    const threshold = image.width / 1.65;
-
-    setIsLeftSide(clickX <= threshold);
-  };
-
-  const handleMouseLeave = () => {
-    setIsLeftSide(true); // Restaurar al salir del área de la imagen
-  };
   return (
-    <div className="container-fluid footer text-white mt-5 pt-5 px-0 position-relative overlay-top">
-      <div className="row mx-0 pt-5 px-sm-3 px-lg-5 mt-4">
-        <div className="col-lg-3 col-md-6 mb-5">
-          <h4
-            className="text-white text-uppercase mb-4"
-            style={{letterSpacing: "3px"}}
-          >
-            Get In Touch
-          </h4>
-          <p>
-            <i className="fa fa-map-marker-alt mr-2"></i>123 Street, New York, USA
-          </p>
-          <p>
-            <i className="fa fa-phone-alt mr-2"></i>+012 345 67890
-          </p>
-          <p className="m-0">
-            <i className="fa fa-envelope mr-2"></i>info@example.com
-          </p>
-        </div>
-        <div className="col-lg-3 col-md-6 mb-5">
-          <h4
-            className="text-white text-uppercase mb-4"
-            style={{letterSpacing: "3px"}}
-          >
-            Follow Us
-          </h4>
-          <p>Amet elitr vero magna sed ipsum sit kasd sea elitr lorem rebum</p>
-          <div className="d-flex justify-content-start">
-            <a className="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a className="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a className="btn btn-lg btn-outline-light btn-lg-square mr-2" href="#">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
-            <a className="btn btn-lg btn-outline-light btn-lg-square" href="#">
-              <i className="fab fa-instagram"></i>
-            </a>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6 mb-5">
-          <h4
-            className="text-white text-uppercase mb-4"
-            style={{letterSpacing: "3px"}}
-          >
-            Open Hours
-          </h4>
-          <div>
-            <h6 className="text-white text-uppercase">Monday - Friday</h6>
-            <p>8.00 AM - 8.00 PM</p>
-            <h6 className="text-white text-uppercase">Saturday - Sunday</h6>
-            <p>2.00 PM - 8.00 PM</p>
-          </div>
-        </div>
-        <div className="col-lg-3 col-md-6 mb-5">
-          <h4
-            className="text-white text-uppercase mb-4"
-            style={{letterSpacing: "3px"}}
-          >
-            Newsletter
-          </h4>
-          <p>Amet elitr vero magna sed ipsum sit kasd sea elitr lorem rebum</p>
-          <div className="w-100">
-            <div className="input-group">
-              <input
-                type="text"
-                className="form-control border-light"
-                style={{padding: "25px"}}
-                placeholder="Your Email"
-              />
-              <div className="input-group-append">
-                <button className="btn btn-primary font-weight-bold px-3">
-                  Sign Up
-                </button>
-              </div>
-            </div>
+    <div className="container-fluid footer text-white mt-5 px-0 position-relative overlay-top">
+      <div
+        className="container-fluid text-center text-white border-top py-4"
+        style={{ borderColor: "rgba(256, 256, 256, .1) !important" }}
+      >
+        <div
+          className="justify-content-between"
+        >
+          <div className="ml-auto p-4" id="footer-images">
+            <NavLink
+              to="/"
+              className="nav-item nav-link"
+              activeClassName="active"
+            >
+              <img src={celsia} />
+            </NavLink>
+            <NavLink
+              to="/departamentos"
+              className="nav-item nav-link"
+              activeClassName="active"
+            >
+              <img src={ecopetrol} />
+            </NavLink>
+            <NavLink
+              to="/tiposEnergia"
+              className="nav-item nav-link"
+              activeClassName="active"
+            >
+              <img src={enel} />
+            </NavLink>
+            <NavLink
+              to="/mapas/general"
+              className="nav-item nav-link"
+              activeClassName="active"
+            >
+              <img src={epm} />
+            </NavLink>
+            <NavLink
+              to="/mapas/general"
+              className="nav-item nav-link"
+              activeClassName="active"
+            >
+              <img src={isagen} />
+            </NavLink>
           </div>
         </div>
       </div>
-      <div
-        className="container-fluid text-center text-white border-top mt-4 py-4 px-sm-3 px-md-5"
-        style={{borderColor: 'rgba(256, 256, 256, .1) !important'}}
-      >
-        <p className="mb-2 text-white">
-          Copyright &copy;{" "}
-          <a className="font-weight-bold" href="#">
-            Grupo PRISA
-          </a>
-          . All Rights Reserved.
-        </p>
+      <div className="row mx-0 px-sm-3 px-lg-5">
+        
       </div>
     </div>
   );
